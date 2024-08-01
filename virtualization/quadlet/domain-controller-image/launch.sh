1a2f4590a5e6aa8cbef1d342bad5d1bc29aaa0e3
Jean-Francois Denise <jdenise@redhat.com>
2024-08-01 14:28:32 +0200

    Add quadlet artifacts

---
+++ virtualization/quadlet/domain-controller-image/launch.sh
@@ -0,0 +1,4 @@
+Domain controller
+sudo /usr/bin/qemu-system-x86_64 -m 8G -accel kvm -cpu host -nographic -netdev user,id=usernet,hostfwd=tcp::9990-:9990,hostfwd=tcp::2222-:22 -device virtio-net,netdev=usernet image/disk.raw
+
+sudo /usr/bin/qemu-system-x86_64 -m 8G -accel kvm -cpu host -nographic -netdev user,id=usernet,hostfwd=tcp::9990-:9990,hostfwd=tcp::2222-:22 -device virtio-net,netdev=usernet image/disk.raw
