697ac1b8a4806f0c61fdd3055fd9580bc3755049
Yeray Borges <yborgess@redhat.com>
2024-09-11 17:38:26 +0100

    Remove bind server address and bind address for management interface. added prostgre sql data base and configuration for the secondary host

--- container/images/run-domain-demo.sh
+++ container/images/run-domain-demo.sh
@@ -10,6 +10,17 @@ podman build -t localhost/host-controller \
 # Create a shared network so all the machines can communicate directly using the host names
 podman network create domain-mode-network
 
+# Start PostgreSQL Database, required for the todo application
+podman run -d --rm \
+  --name postgresql \
+  --hostname database-server \
+  --network domain-mode-network \
+  -p 5432:5432 \
+  -e POSTGRES_USER=todos-db \
+  -e POSTGRES_PASSWORD=todos-db \
+  -e POSTGRES_DB=todos-db \
+  docker.io/postgres:16
+
 # Run the domain-controller container
 podman run -d --rm \
   --name domain-controller \
@@ -21,10 +32,12 @@ podman run -d --rm \
   -e JBOSS_EAP_DOMAIN_PASSWORD=secondary \
   -e JBOSS_EAP_DOMAIN_DOMAIN_CONFIG=openshift-domain.xml \
   -e JBOSS_EAP_DOMAIN_HOST_CONFIG=my-host-primary.xml \
-  -e SERVER_PUBLIC_BIND_ADDRESS=0.0.0.0 \
-  -e SERVER_MANAGEMENT_BIND_ADDRESS=0.0.0.0 \
   --label mode=domain-controller \
   localhost/domain-controller
+
+# Wait for the domain controller to start, just to make sure it's ready and the host controller can join the domain
+# on the first try
+sleep 2
 
 # Run the secondary-host container
 podman run -d --rm \
@@ -37,8 +50,12 @@ podman run -d --rm \
   -e JBOSS_EAP_DOMAIN_PASSWORD=secondary \
   -e JBOSS_EAP_DOMAIN_PRIMARY_ADDRESS=primaryhostname \
   -e JBOSS_EAP_DOMAIN_HOST_CONFIG=my-host-secondary.xml \
-  -e SERVER_PUBLIC_BIND_ADDRESS=0.0.0.0 \
-  -e SERVER_MANAGEMENT_BIND_ADDRESS=0.0.0.0 \
+  -e POSTGRESQL_DATABASE=todos-db \
+  -e POSTGRESQL_USER=todos-db \
+  -e POSTGRESQL_PASSWORD=todos-db \
+  -e POSTGRESQL_DATASOURCE=ToDos \
+  -e POSTGRESQL_SERVICE_HOST=database-server \
+  -e POSTGRESQL_SERVICE_PORT="5432" \
   --label mode=host-controller \
   localhost/host-controller
 
